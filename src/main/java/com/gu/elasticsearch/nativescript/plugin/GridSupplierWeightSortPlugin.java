7bcb743e30f821dd57b9f8cd4eec14d51a0b130a
Robert Kenny <robert.kenny@guardian.co.uk>
2016-07-15 11:52:41 +0100

    register dummy script

--- src/main/java/com/gu/elasticsearch/nativescript/plugin/GridSupplierWeightSortPlugin.java
+++ src/main/java/com/gu/elasticsearch/nativescript/plugin/GridSupplierWeightSortPlugin.java
@@ -2,7 +2,7 @@ package com.gu.elasticsearch.nativescript.plugin;
 
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.script.ScriptModule;
-
+import com.gu.elasticsearch.nativescript.script.GridSupplierWeightSortScript;
 
 public class GridSupplierWeightSortPlugin extends AbstractPlugin {
     @Override
@@ -13,5 +13,9 @@ public class GridSupplierWeightSortPlugin extends AbstractPlugin {
     @Override
     public String description() {
         return "Elasticsearch Native Plugin for Guardian Grid image supplier weighted sort";
+    }
+
+    public void onModule(ScriptModule module) {
+        module.registerScript("grid_supplier_weight_sort", GridSupplierWeightSortScript.Factory.class);
     }
 }
